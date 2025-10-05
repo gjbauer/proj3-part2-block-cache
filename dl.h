2f331b85e5341f6f102e7adb3e790bd45ffe8ec1
Gabriel Bauer <gabeb1277@gmail.com>
2025-10-05 00:35:02 -0400

    dirty list

---
+++ dl.h
@@ -0,0 +1,26 @@
+#ifndef DL_H
+#define DL_H
+#include "config.h"
+
+/* There is no reason in particular why our hashhap has to be the same size as our cache,
+ * other than simply not having defined another macro...which we can. But, we set our indices
+ * equal to our cache size in case if one file happens to take up our entire cache...unlikely
+ * but certainly within the realm of possibility. */
+
+struct DL_LL
+{
+	int key;
+	int indices[CACHE_SIZE];
+	struct DL_LL *next;
+};
+
+typedef struct DL_HM
+{
+	struct DL_LL HashMap[CACHE_SIZE];
+} DL_HM;
+
+int dl_lookup(DL_HM *hashmap, int key);
+void dl_insert(DL_HM *hashmap, int key, int index);
+void dl_delete(DL_HM *hashmap, int key);
+
+#endif
--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 #include "pci.h"
 #include "fl.h"
 #include "cache.h"
+#include "dl.h"
 
 int main()
 {
-
+	// TODO: Implement functions for our data structures and test them here...
+	// TODO: After testing, integrate it with our B-Tree...
 }
 
